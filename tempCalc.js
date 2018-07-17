6bc78a023459f7a27fd2fd1cce9bde66d315f2fb
Your Name <fernandez.Leovanny@gmail.com>
2018-07-17 19:23:48 -0400

    second commit

---
+++ tempCalc.js
@@ -0,0 +1,21 @@
+function convertToC(num){
+
+  let c = (num * (9/5)) + 32;
+
+  return c;
+}
+
+function convertToF(num){
+  let f = (num - 32) * 5/9;
+
+  return f
+
+}
+
+
+
+
+
+
+  module.exports = convertToF;
+  module.exports = convertToC;
--- test/spec.js
+++ test/spec.js
@@ -1,4 +1,27 @@
 const expect = require('chai').expect;
-const tempCalc = require('../tempCalc.js');
+const convertToF = require('../tempCalc.js');
+const convertToC= require('../tempCalc.js');
 
-describe
+describe('has a convert F function', ()=>{
+  it('exist',()=>{
+    expect(convertToF).to.be.ok;
+  });
+  it('can convert base case at 0',()=>{
+    expect(convertToF(0)).to.be.equal(86);
+  });
+  xit('can conver more other nums', ()=>{
+    expect(convertToF(40)).to.be.equal(4.44);
+  });
+});
+
+describe('has a convert C function', ()=>{
+  it('exist', ()=>{
+    expect(convertToC).to.be.ok;
+  });
+  it('can handle the base case 0', ()=>{
+    expect(convertToC(0)).to.be.equal(32);
+  });
+  it('-10', ()=>{
+    expect(convertToC(-10)).to.be.equal(14)
+  });
+});
